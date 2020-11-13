a9aa71ff0e043aed76152b616dd200f4cbeff830
Victor Fernando Dev <victor-fernando-15@hotmail.com>
2020-11-13 10:39:20 -0300

    Initial commit

---
+++ CursoMVC/Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        [Display(Name="Descricao")]
+        [Required(ErrorMessage ="O campo é obrigatório")]
+        public string Descricao { get; set; }
+        public List<Produto> Produto { get; set; }
+    }
+}
---
+++ CursoMVC/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descricao")]
+        public string Descricao { get; set; }
+        [Range(1, 10, ErrorMessage = "Valor fora da faixa")]
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
